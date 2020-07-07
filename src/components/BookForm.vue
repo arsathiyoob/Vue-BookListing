c497c5b2ecffb889152a51a1745fd09886491a3a
Iyoob <mohamed-arsath.iyoob@capgemini.com>
2020-07-07 13:26:52 +0530

    Completed module

---
+++ src/components/BookForm.vue
@@ -0,0 +1,26 @@
+<template>
+<form class="" action="#" method="post">
+    <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
+    <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <button type="submit" name="button">Add Book</button>
+    <submit type='method' name='bookSubmit'>{{ bookTitle }}: {{ bookAuthor }}]</submit>
+</form>
+</template>
+<script>
+export default {
+name: 'BookForm',
+props: ['books']
+}
+data(){
+  bookTitle: '',
+  bookAuthor: ''
+},
+methods: {
+bookSubmit(bookItem,bookAuthor){
+  addBook: this.$emit('addBook', bookTitle, bookAuthor);
+}
+},
+</script>
+<style>
+
+</style>
